c713626ceb1c633b1b1eddab8b18e35a6de3f257
Adam Halka <galkaadam4@gmail.com>
2026-02-15 14:03:36 +0100

    Solution

--- src/routes/accounts.py
+++ src/routes/accounts.py
@@ -52,8 +52,8 @@ async def register(user_to_add: UserRegistrationRequestSchema, db: AsyncSession 
 
     try:
         new_user = UserModel.create(
-            email=user_to_add["email"],
-            raw_password=user_to_add["password"],
+            email=user_to_add.email,
+            raw_password=user_to_add.password,
             group_id=user_group.id,
         )
         activation_token = ActivationTokenModel(user=new_user)
@@ -111,6 +111,7 @@ async def activate(data: UserActivationRequestSchema, db: AsyncSession = Depends
         db_user.is_active = True
         await db.delete(db_token)
         await db.commit()
+        await db.refresh(db_user)
         return {"message": "User account activated successfully."}
 
     except Exception:
