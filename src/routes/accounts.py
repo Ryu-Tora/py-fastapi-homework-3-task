27fabe1e2de1b567011a1744fe8b48f20d4960fe
Adam Halka <galkaadam4@gmail.com>
2026-02-15 13:11:46 +0100

    Solution

--- src/routes/accounts.py
+++ src/routes/accounts.py
@@ -208,9 +208,7 @@ async def login(
             raise HTTPException(status_code=403, detail="User account is not activated.")
 
         access_token = jwt_manager.create_access_token(
-            data={
-                "user_id": str(db_user.id),
-                }
+            data={"user_id": str(db_user.id)}
         )
         refresh_token_str = jwt_manager.create_refresh_token(
             {"user_id": str(db_user.id)}
